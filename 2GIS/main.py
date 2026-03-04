399059550b520ed39e3221cb8c74655d9af10a3e
AlexeyKuzko <kuzkoalexey@icloud.com>
2026-03-04 21:04:43 +0300

    solved

--- 2GIS/main.py
+++ 2GIS/main.py
@@ -20,30 +20,29 @@
 # минимальная высота / ширина - 3х3
 # ограничения по тексту - максимальная длина 120, минимальная 0
 
-def draw_frame(func):
-    def wrapper(w, h):
-        result = func(w, h)
-        w_border = "#" * h
-        print(w_border) # upper
 
-        h_border =
-
-        print(result)
-
-        if w_border:
-            print(w_border)
-    return wrapper
-
-
-@draw_frame
-def print_in_box(text, width, height):
-    if 3 < width:
-        raise Exception("Минимальная ширина 3")
-    elif 3 < height:
-        raise Exception("Минимальная длина 3")
+def print_in_box(text: str, width: int, height: int):
+    if width < 3 or height < 3:
+        raise ValueError("Минимальный размер рамки 3x3")
     if len(text) > 120:
-            raise Exception("Максимальная длина текста 120")
-    return f"{text}"
+        raise ValueError("Максимальная длина текста 120")
+    if len(text) > width - 2:
+        raise ValueError("Текст не помещается в рамку")
+    print("#" * width)
+    inner_height = height - 2
+    text_row = inner_height // 2
+    for i in range(inner_height):
+        if i == text_row:
+            padding = width - 2 - len(text)
+            left = padding // 2
+            right = padding - left
+            print("#" + " " * left + text + " " * right + "#")
+        else:
+            print("#" + " " * (width - 2) + "#")
+    print("#" * width)
 
 
-print_in_box("Hello, world!", 17, 5)
+print_in_box("Hello, World!", 17, 5)
+print_in_box("", 3, 3)
+print_in_box("HelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHello"
+             "Hello!!!!Hello!!!!", 120, 120)
